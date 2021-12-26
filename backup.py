2fc2c0f63b9a5fd86843a4c5d07e74ffd94199e5
AmeliaYeah <87606015+AmeliaYeah@users.noreply.github.com>
2021-12-26 09:38:32 -0500

    file permissions are now preserved

--- backup.py
+++ backup.py
@@ -202,7 +202,7 @@ def compile():
     #restore their files (and ensure the dependancies are installed)
     apt update
     apt install -y {' '.join(required_packages)}
-    unzip back.zip
+    unzip back.zip -X -K
     rsync -avhu --progress ./files-backup /
 
     #clean up
